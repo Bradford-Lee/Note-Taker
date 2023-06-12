87c2066ead74f27d31419688781eb768f3b9af91
Bradford <bzlee@ucdavis.edu>
2023-06-11 20:40:50 -0700

    added Get and Post routes, added helpers folder, downloaded npm packages

---
+++ server.js
@@ -0,0 +1,74 @@
+// import NPM packages & public folder
+const express = require('express');
+const path = require('path');
+const api = require('./public/assets/js/index.js');
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+// listen on 3001 port
+const PORT = process.env.port || 3001;
+
+// create new server to configure
+const app = express();
+
+const notes = require('/db/notes.json')
+const fs = require('fs')
+
+// Middleware for parsing JSON and urlencoded form data, front end gets to back end on request data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// setting up routes
+app.use('/api', api);
+
+// look for files in public folder
+app.use(express.static('public'));
+
+// GET Route for retrieving all the notes
+// /api/db
+notes.get('/', (req, res) => {
+    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(note)));
+});
+
+// POST
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/notes.json');
+        res.json('Note added successfully');
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+// DELETE
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/notes.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((note) => note.note_id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/notes.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+  });
+
+// module.exports = notes;
